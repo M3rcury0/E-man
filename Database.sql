a2d7ec15a6d8f717a580c7d6d6415859f1dd47a0
Himal Sah <sahhimal7@gmail.com>
2024-12-31 22:52:08 +0530

    Review 2

--- Database.sql
+++ Database.sql
@@ -1,58 +1,84 @@
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: empms
+-- ------------------------------------------------------
+-- Server version	8.0.40
+Create database IF NOT EXISTS empms;
+Use empms;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `employee`
 --
 
-CREATE TABLE employee (
-  id int NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employee` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
-  age int NOT NULL,
-  manager_id int DEFAULT NULL,
-  PRIMARY KEY (id),
-  KEY manager_idx (manager_id),
-  CONSTRAINT manager FOREIGN KEY (manager_id) REFERENCES manager (id)
+  `age` int NOT NULL,
+  `manager_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `manager_idx` (`manager_id`),
+  CONSTRAINT `manager` FOREIGN KEY (`manager_id`) REFERENCES `manager` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `employee`
+--
 
-
-LOCK TABLES employee WRITE;
-/*!40000 ALTER TABLE employee DISABLE KEYS */;
-INSERT INTO employee VALUES (1,'Rakesh Chaudhary ',56,2),(2,'Suman Mishra',21,1),(3,'Anil Joshi',29,3),(4,'nandita Gupta',32,5),(5,'Ashwin Tewari',40,2),(6,'Ritu Menon',25,1),(7,'Prakash Desai',50,4),(8,'Manoj Verma',22,3),(9,'Abhishek Ghosh',39,1),(10,'Meera Patil',41,4);
-/*!40000 ALTER TABLE employee ENABLE KEYS */;
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES (1,'Rakesh Chaudhary ',56,2),(2,'Suman Mishra',21,1),(3,'Anil Joshi',29,3),(4,'Nandita Gupta',32,5),(5,'Ashwin Tewari',40,2),(6,'Ritu Menon',25,1),(7,'Prakash Desai',50,4),(8,'Manoj Verma',22,3),(9,'Abhishek Ghosh',39,1),(10,'Meera Patil',41,4);
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
 -- Table structure for table `manager`
 --
 
-DROP TABLE IF EXISTS manager;
+DROP TABLE IF EXISTS `manager`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE manager (
-  id int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `manager` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(60) NOT NULL,
-  department varchar(225) NOT NULL,
-  PRIMARY KEY (id)
+  `department` varchar(225) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `manager`
+--
 
-LOCK TABLES manager WRITE;
-/*!40000 ALTER TABLE manager DISABLE KEYS */;
-INSERT INTO manager VALUES (1,'Manohar Sing','HR'),(2,'Priya Patel','Accounting'),(3,'Sanjay Bhatt','Sales'),(4,'Jyoti Nair','IT'),(5,'Sunita Nair','Marketing');
-/*!40000 ALTER TABLE manager ENABLE KEYS */;
+LOCK TABLES `manager` WRITE;
+/*!40000 ALTER TABLE `manager` DISABLE KEYS */;
+INSERT INTO `manager` VALUES (1,'Manohar Sing','HR'),(2,'Priya Patel','Accounting'),(3,'Sanjay Bhatt','Sales'),(4,'Jyoti Nair','IT'),(5,'Sunita Nair','Marketing');
+/*!40000 ALTER TABLE `manager` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
 --
 
-CREATE TABLE users (
-  username varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  PRIMARY KEY (username),
-  UNIQUE KEY username_UNIQUE (username)
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`username`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -60,11 +86,18 @@ CREATE TABLE users (
 -- Dumping data for table `users`
 --
 
-LOCK TABLES users WRITE;
-/*!40000 ALTER TABLE users DISABLE KEYS */;
-INSERT INTO users VALUES ('bhoothumein98','asdasd4as2069'),('jadoo24','asdasdw8715'),('meera99','asdasd541'),('nameisghosh','ghjfhd4454s'),('suman@24','eteawsrfdasdfc65'),('therajesh54','asdcrgghreut96'),('yashking_55','cvbxcx2');
-/*!40000 ALTER TABLE users ENABLE KEYS */;
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('bhoothumein98','asdasd4as2069'),('jadoo24','asdasdw8715'),('meera99','asdasd541'),('nameisghosh','ghjfhd4454s'),('suman@24','eteawsrfdasdfc65'),('therajesh54','asdcrgghreut96'),('yashking_55','cvbxcx2');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
+-- Dump completed on 2024-12-31 19:05:11
