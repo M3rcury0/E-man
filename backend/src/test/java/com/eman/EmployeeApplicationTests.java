838e6f2741090c5d3319ac51a5a67f873fdab108
Himal Sah <sahhimal7@gmail.com>
2024-12-31 08:33:45 +0530

    Absolutely losing it

--- backend/src/test/java/com/eman/EmployeeApplicationTests.java
+++ backend/src/test/java/com/eman/EmployeeApplicationTests.java
@@ -2,12 +2,11 @@ package com.eman;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
-@TestPropertySource(locations = "classpath:application.properties")
 class EmployeeApplicationTests {
 
     @Test
